ea45769f69f5ca49da7a7957e7ce82833c606e23
VINAYLAM07 <vinay.lam19257@gmail.com>
2026-07-31 21:41:03 +0530

    patterns cont.

--- src/main/java/com/example/dsa/patterns/pattern1.java
+++ src/main/java/com/example/dsa/patterns/pattern1.java
@@ -41,18 +41,71 @@ public class pattern1 {
          * *
          *
          * */
-        pattern6(5);
+//        pattern6(5);
+//        pattern66(10);
+        /*
+            *
+           * *
+          * * *
+         * * * *
+        * * * * *
+         * * * *
+          * * *
+           * *
+            *
+        */
+        pattern7(4);
+/*
+             1
+           2 1 2
+         3 2 1 2 3
+       4 3 2 1 2 3 4
+*/
+
     }
 
-    private static void pattern6(int n) {
+    private static void pattern7(int n) {
+        for(int row=0; row<n;row++){//increasing row size for revers stars
+
+            int starsCount = row+1;//until <n taking row as a no of starts to print after  crossing it taking extended row limit - row
+            int spaces = n - starsCount;
+            for(int col=0;col<spaces;col++){
+                System.out.print("  ");
+            }
+            for(int col=starsCount;col>1;col--){
+                System.out.print(col+" ");
+            }
+            for(int col=1;col<=starsCount;col++){
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    private static void pattern6(int n) { //my own thinking 😅
         for(int row=0; row<2*n;row++){//increasing row size for revers stars
             int rev =1;
-            int spaces = (row <n) ? n-row : (n-rev);
-            for(int col=0;col<=spaces;col++){
+            int spaces = (row <n) ? n-row-1 : (row-n+1);
+            for(int col=0;col<spaces;col++){
                 System.out.print(" ");
                 rev++;
             }
-            int starsCount = (row<n) ? row : (2*n -row);//until <n taking row as a no of starts to print after  crossing it taking extended row limit - row
+            int starsCount = (row<n) ? row+1 : (2*n -row-1);//until <n taking row as a no of starts to print after  crossing it taking extended row limit - row
+            for(int col=0;col<starsCount;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    private static void pattern66(int n) { //mindblock thinking 😲
+        for(int row=0; row<2*n;row++){//increasing row size for revers stars
+
+            int starsCount = (row<n) ? row+1 : (2*n -row-1);//until <n taking row as a no of starts to print after  crossing it taking extended row limit - row
+            int spaces = n - starsCount;
+            for(int col=0;col<spaces;col++){
+                System.out.print(" ");
+            }
             for(int col=0;col<starsCount;col++){
                 System.out.print("* ");
             }
@@ -62,7 +115,7 @@ public class pattern1 {
 
     private static void pattern5(int n) {
         for(int row=0; row<2*n;row++){//increasing row size for revers stars
-            int starsCount = (row<n) ? row : (2*n -row);//until <n taking row as a no of starts to print after  crossing it taking extended row limit - row
+            int starsCount = (row<n) ? row+1 : (2*n -row-1);//until <n taking row as a no of starts to print after  crossing it taking extended row limit - row
             for(int col=0;col<starsCount;col++){
                 System.out.print("* ");
             }
